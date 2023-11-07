e552598ce69aef8815e82d0d2f97a423a89ef135
YingSSY <148355363+YingSSY@users.noreply.github.com>
2023-11-08 01:50:03 +0800

    insert order + css for round button

--- insert_order.php
+++ insert_order.php
@@ -9,13 +9,13 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['productid']) && isset
     // $totalprice = $price * $quantity;
 
     // Add var_dump to check the input
-    echo 'Input Data:';
-    var_dump($_POST);
+    // echo 'Input Data:';
+    // var_dump($_POST);
 
     if (!isset($_SESSION['order'])) {
         $_SESSION['order'] = array();
     }
-    
+
     $_SESSION['order'][$productid] = array(
         'productid' => $productid,
         'productname' => $productname,
@@ -23,30 +23,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['productid']) && isset
         'quantity' => $quantity
     );
 
-    // if (!isset($_SESSION['order'][$productid])) {
-    //     $_SESSION['order'][$productid] = [
-    //         'productname' => $productname,
-    //         'price' => $price,
-    //         'quantity' => $quantity
-    //     ];
-    // } else {
-    //     $_SESSION['order'][$productid] += [
-    //         'productname' => $productname,
-    //         'price' => $price,
-    //         'quantity' => $quantity
-    //     ];
-    // }
+    $subtotal = 0;
+      foreach ($_SESSION['order'] as $item) {
+          $subtotal += $item['price'] * $item['quantity'];
+      }
+      echo 'Subtotal: $' . number_format($subtotal, 2);
 
     // Output the session data for testing
-    echo 'Using var_dump:';
+    // echo 'Using var_dump:';
     var_dump($_SESSION);
 
     // Redirect back to the previous page or wherever you want to go
-    // header("Location: checkOut.php");
-    // echo '<script>
-    //     window.location.href = document.referrer; // Redirect to the previous page
-    //     alert("Item has been added to the cart."); // Display an alert
-    // </script>';
+    header("Location: checkOut.php");
+    echo '<script>
+        window.location.href = document.referrer; // Redirect to the previous page
+        alert("Item has been added to the cart."); // Display an alert
+    </script>';
 
     exit();
 }
